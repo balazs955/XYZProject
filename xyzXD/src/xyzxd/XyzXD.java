e9c3c0b538292779149dbe570dec27408a2a554d
balazs955 <83276057+balazs955@users.noreply.github.com>
2022-11-08 12:49:27 +0100

    Add files via upload

---
+++ xyzXD/src/xyzxd/XyzXD.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xyzxd;
+import java.util.Scanner;
+/**
+ *
+ * @author user3
+ */
+public class XyzXD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Kérem a henger sugarát: ");
+        int sugar = myObj.nextInt();
+        System.out.println("Kérem a henger magasságát: ");
+        int magassag = myObj.nextInt();
+       
+    }
+    
+}
